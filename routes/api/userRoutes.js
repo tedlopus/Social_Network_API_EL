20005e5f20c79ff9edbe71aabb08ced3f6c22da5
tedlopus <tedlopus@gmail.com>
2022-05-25 23:08:42 -0600

    deleted seed and data files in utils folder per README instructions to seed from insomnia, updated routes to reflect addFriend and removeFriend controllers and installed nodemon

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,8 +7,8 @@ const {
     getSingleUser,
     createUser,
     deleteUser,
-    addThought,
-    removeThought,
+    addFriend,
+    removeFriend,
 } = require('../../controllers/userController');
 
 // /api/users/
@@ -17,11 +17,11 @@ router.route('/').get(getUsers).post(createUser);
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser).delete(deleteUser);
 
-// /api/users/:userId/thoughts
-router.route('/:userId/thoughts').post(addThought);
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
 
-// /api/users/:userId/thoughts/:thoughtId
-router.route('/:userId/thoughts/:thoughtId').delete(removeThought);
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').delete(removeFriend);
 
 // export the router
 module.exports = router;
